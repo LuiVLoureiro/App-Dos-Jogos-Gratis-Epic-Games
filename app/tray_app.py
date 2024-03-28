077b4b914ca7a77ac0b0f3d8a7756cbfc07fbc28
LuiLoureiro <luivloureiro@gmail.com>
2024-03-28 19:15:35 -0300

    ftr: Estilizar app

--- app/tray_app.py
+++ app/tray_app.py
@@ -9,6 +9,8 @@ def mostrar_janela(icon, item):
         mostrar_janela.janela.title("Dev Lui")
         mostrar_janela.janela.configure(bg='#333333')
         mostrar_janela.janela.overrideredirect(True)
+        canvas = tk.Canvas(mostrar_janela.janela, width=300, height=1)
+        
 
         # Configura o tamanho da janela
         largura_janela = 250
@@ -23,7 +25,10 @@ def mostrar_janela(icon, item):
         mostrar_janela.janela.geometry(f'+{x_posicao - 50}+{y_posicao - 70}')
 
         label = tk.Label(mostrar_janela.janela, text="JOGOS GRATIS EPIC GAMES", bg='#333333', font='impact', fg='#ffffff', pady='30')
+        
         label.pack()
+        canvas.create_line(1, 1, 1, 1, fill='white')
+        canvas.pack()
 
         # Define uma ação para quando a janela for fechada
         mostrar_janela.janela.protocol("WM_DELETE_WINDOW", fechar_janela)
