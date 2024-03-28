a496581b5d363c1f691a023b41036a04cab9e7c8
LuiLoureiro <luivloureiro@gmail.com>
2024-03-28 08:39:13 -0300

    ftr: Pesquisar jogos no google para econtrar link na steam

--- baseline/main.py
+++ baseline/main.py
@@ -10,6 +10,7 @@ class Scrapper:
         pass
 
     def obter_nomes():
+        global jogos_gratis
         # Conseguir nome dos jogos na epic
         url = requests.get('https://store-site-backend-static-ipv4.ak.epicgames.com/freeGamesPromotions?locale=pt-BR&country=BR&allowCountries=BR')
         dados = str(url.text)
@@ -24,6 +25,17 @@ class Scrapper:
 
         # Adicionar ao array 'Jogos Gratis' apenas os jogos que estiverem com o preço igual a 0
         jogos_gratis = titulos[np.where(precos_desconto == 0)]
-        print(jogos_gratis)
+
+    
+    def pesquisar_jogos():
+        for i in range(len(jogos_gratis)):
+            url_pesquisa = requests.get(f'https://www.google.com/search?q={jogos_gratis[i]}+steam')
+            soup_pesquisa = BeautifulSoup(url_pesquisa.text, 'html.parser')
+            links = soup_pesquisa.find_all('a', attrs={'data-ved': True})
+            href = [link['href'] for link in links]
+            href = href[0][7:]
+
+            print(href)
 
 Scrapper.obter_nomes()
+Scrapper.pesquisar_jogos()
